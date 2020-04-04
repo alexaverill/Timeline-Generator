b19c0947685cad4abc1bc8f383296fc0a74c07c0
Alex <alex.averill2013@gmail.com>
2020-04-04 12:47:29 -0600

    Cleaning up code, fixing math

--- main.js
+++ main.js
@@ -48,11 +48,7 @@
                 this.content = content;
             }
         }
-        var timeline = new Timeline();
-        function loaded() {
-            console.log("Loaded");
-            //generateTimeline();
-        }
+        
         function getRange(earliest,latest){
             let years = latest.getFullYear()-earliest.getFullYear();
             let range  = 0;
@@ -72,47 +68,78 @@
             }
             return r;
         }
-        //TODO maybe refactor.
-        function generateTimeline(timeline) {
-            console.log(timeline.numMonths);
-            let cssCode = document.getElementById('cssCode');
-            let htmlCode = document.getElementById('htmlCode');
-
-            let months = timeline.numMonths;
-             let tempRows = calculateTemplateRows(months);
-            console.log(months);
-            let timelineCSS = `height:50vh;\n
-                               display: grid;\n 
-                               grid-template-columns:48% 1% auto;\n
-                               grid-template-rows:${tempRows};\n`;
-            let barCSS = `grid-column: 2/3;
-                          grid-row: 1/${months};
+        function makeBarCSS(numberMonths,mobile=false){
+            let gridCol = "2/3";
+            if(mobile){
+                gridCole = "1/2"
+            }
+            let barCSS = `grid-column: ${gridCol};
+                          grid-row: 1/${numberMonths};
                           border-radius: 10px;
                           background-color: #49a078;`;
-            let cssString = `.timeline{${timelineCSS}}\n;
-                             .bar{${barCSS}} `;
+            return barCSS;
+        }
+        function makeTimelineCSS(rows,mobile=false){
+            let cols = "48% 1% auto";
+            if(mobile){
+                cols = "1% 48% auto"
+            }
+            let timeline = `height:50vh;\n
+            display: grid;\n 
+            grid-template-columns:${cols};\n
+            grid-template-rows:${rows};\n`;
+            return timeline;
+        }
+        function generateCSS(timeline,mobile=false){
+            let months = timeline.numMonths;
+            let tempRows = calculateTemplateRows(months);
+            let timelineCSS = makeTimelineCSS(tempRows);
+            let barCSS = makeBarCSS();
+            let cssString = `.timeline{\n${timelineCSS}}\n;
+                             .bar{${barCSS}\n} \n`;
             let cssEntries = [];
             let newestYear = timeline.entries[timeline.entries.length-1].date.getFullYear();
             for(let x = 0 ; x< timeline.entries.length; x++){
-                let m = timeline.entries[x].date.getMonth();
-                let y = timeline.entries[x].date.getFullYear();
-                let diff = timeline.numYears - (newestYear-y);
-                let inverseMonths = 12 * diff;
-                let startCol = timeline.numMonths-(m+inverseMonths);
-                //console.log(m+" "+inverseMonths +" "+(timeline.numMonths-(m+inverseMonths)));
-                let rowPos = (startCol-1 ) + "/" + (startCol );
+                let currentMonth = timeline.entries[x].date.getMonth();
+                let currentYear = timeline.entries[x].date.getFullYear();
+                let diff = timeline.numYears - (newestYear-currentYear);
+                let monthNumber = 12 * diff;
+                let startCol = currentMonth+monthNumber;
+                startCol = timeline.numMonths-startCol+1;
+                console.log(timeline.numMonths);
+                console.log(timeline.entries[x].date + " " +currentMonth+" "+monthNumber +" "+startCol);
+                let rowPos = (startCol ) + "/" + (startCol+1);
                 let colPos = "1/2";
                 let alignment = "right";
                 if (x % 2 == 0) {
                     colPos = "3/4";
                     alignment = "left";
                 }
-                let innerCSS = `text-align:${alignment};\npadding:10;\ngrid-column:${colPos };\ngrid-row:${rowPos};\n`;
+                if(mobile){
+                    colPos = "2/3";
+                    alignment="left";
+                }
+                let innerCSS = `text-align:${alignment};\n
+                            adding:10;\n
+                            grid-column:${colPos };\n
+                            grid-row:${rowPos};\n`;
                 cssEntries.push(innerCSS);
                 cssString += `.t${x + 1}{\n${innerCSS}}\n`;
             }
-            cssCode.innerText = cssString;
+            return [cssString,cssEntries];
+        }
+        //TODO maybe refactor.
+        function generateTimeline(timeline,mobile=false) {
+            console.log(timeline.numMonths);
+            let cssCode = document.getElementById('cssCode');
+            let htmlCode = document.getElementById('htmlCode');
+            let css = generateCSS(timeline);
+            cssCode.innerText = css[0];
+            let cssEntries = css[1];
             //generate HTML for elements
+            let tempRows = calculateTemplateRows(timeline.numMonths);
+            let timelineCSS = makeTimelineCSS(tempRows);
+            let barCSS = makeBarCSS();
             let htmlString = `<div class="timeline" id="timeline" style="${timelineCSS}"><div class="bar" style="${barCSS}"></div>`;
             let outputHTML = '<div class="timeline" id="timeline"><div class="bar"></div>';
 
@@ -181,25 +208,27 @@
         }
         function processEntries() {
             let entries = document.getElementsByClassName("entry");
-            //console.log(entries);
-            //timelineEntries = [];
+            let timeline = new Timeline();
             for (let x = 0; x < entries.length; x++) {
                 let inputs = entries[x].getElementsByTagName('input');
                 let time = inputs[0].value;
                 let content = inputs[1].value;
+                console.log(time);
                 timeline.addEntry(new TimeElement(new Date(time), content));
             }
-
-
-            generateTimeline(timeline.getEntries());
+            timeline.sort();
+            generateTimeline(timeline);
         }
         function loaded(){
-            timeline.addEntry(new TimeElement(new Date('1/22/2018'),'a'));
-            //timeline.addEntry(new TimeElement(new Date('2/22/2018'),'a'));
-            timeline.addEntry(new TimeElement(new Date('3/12/2018'),'a'));
-            timeline.addEntry(new TimeElement(new Date('1/12/2019'),'a'));
-            timeline.addEntry(new TimeElement(new Date('11/02/2019'),'a'));
-            generateTimeline(timeline);
+            let timeline = new Timeline();
+             timeline.addEntry(new TimeElement(new Date('04/01/2020'),'a'));
+            // timeline.addEntry(new TimeElement(new Date('3/12/2018'),'a'));
+             timeline.addEntry(new TimeElement(new Date('01/12/2020'),'a'));
+            // timeline.addEntry(new TimeElement(new Date('1/22/2018'),'a'));
+            timeline.addEntry(new TimeElement(new Date('2/22/2019'),'a'));
+            
+
+             generateTimeline(timeline);
             // let a = new Date('1/22/2020');
             // let b = new Date('2/21/2019');
             // let c = new Date('07/08/2018');
